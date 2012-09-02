c51ffcbef88aea232d920be431a34693bc025be5
Daniel Gullberg <daniel_gullberg@hotmail.com>
2012-09-02 22:03:49 +0200

    Working P controller!
    But why does PWM work from ecmd without enabling it in menuconfig? When
    trying to call setpwm from within my code, it doesn't work despite
    having included pwm.h. When I enable it in menuconfig it stops working!
    I had to add the pinning in netio.m4

--- pinning/hardware/netio.m4
+++ pinning/hardware/netio.m4
@@ -1,5 +1,6 @@
 /* port the enc28j60 is attached to */
 pin(SPI_CS_NET, SPI_CS_HARDWARE)
+pin(CHANNEL_B_PWM, PD4, OUTPUT)
 
 ifdef(`conf_SD_READER', `dnl
   /* port the sd-reader CS is attached to */
--- services/heating_controller/heating_ctrl.c
+++ services/heating_controller/heating_ctrl.c
@@ -1,4 +1,4 @@
- /*  Heating controller for home automation
+/*  Heating controller for home automation
 
  Copyright(C) 2012 Daniel Gullberg <daniel_gullberg@hotmail.com>
 
@@ -15,7 +15,7 @@
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software Foundation,
  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ */
 
 #include <avr/pgmspace.h>
 
@@ -25,21 +25,23 @@
 
 #include "config.h"
 #include "heating_ctrl.h"
+#include "core/bit-macros.h"
 
 #include "hardware/onewire/onewire.h"
+#include "hardware/pwm/pwm.h"
 
 #include "protocols/ecmd/ecmd-base.h"
 
 /*
   If enabled in menuconfig, this function is called during boot up of ethersex
-*/
+ */
 int16_t
 heating_ctrl_init(void)
 {
-	HEATINGCTRLDEBUG ("init\n");
-  // enter your code here
+        HEATINGCTRLDEBUG ("init\n");
+        // enter your code here
 
-  return ECMD_FINAL_OK;
+        return ECMD_FINAL_OK;
 }
 
 /*
@@ -48,66 +50,85 @@ heating_ctrl_init(void)
 
   Sensors:
   "a":["105602a501080011",
-		"Radiatorer, tillopp"],
+                "Radiatorer, tillopp"],
 "tIn":["28dbfa7102000051",
-		  "Inomhus, nere"],
+                  "Inomhus, nere"],
 "d":["10d136a5010800e5",
-	"Ventilation, uteluft"],
-*/
+        "Ventilation, uteluft"],
+ */
 int16_t
 heating_ctrl_periodic(void)
 {
-	int16_t	tIndoor, tOutdoor, tRad; // Measured temperatures
-	int16_t tTargetIndoor, tTargetRad; // Target temperatures
-	int16_t uShunt;
+        int16_t	tIndoor, tOutdoor, tRad; // Measured temperatures
+        int16_t tTargetIndoor, tTargetRad; // Target temperatures
+        int16_t uShunt;
+        uint8_t addr[8] = {0x10,0x56,0x02,0xa5,0x01,0x08,0x00,0x11};
 
-	int8_t ret;
-	//const ow_rom_code_t romSensorRad = {0x105602a501080011};
-	ow_rom_code_t romSensorRad;
-	HEATINGCTRLDEBUG("periodic\n");
-	romSensorRad.raw = 0x105602a501080011;
-	tTargetRad = 40;
-	ret=ow_temp_start_convert_wait(&romSensorRad);
-	HEATINGCTRLDEBUG ("conv %d\n",ret);
+        uint16_t i;
 
-	ow_temp_scratchpad_t sp;
-	ret = ow_temp_read_scratchpad(&romSensorRad, &sp);
-
-	if (ret != 1)
-	{
-	HEATINGCTRLDEBUG("scratchpad read failed: %d\n", ret);
-	}
-	else{	HEATINGCTRLDEBUG("successfully read scratchpad\n");
-
-	}
-
-    //self.s.send("1w convert" + "\n")
-    //self.s.send("1w get " + sensorID + "\n")
-    //response = float(self.s.recv(1024).rstrip("\n").lstrip())
-
-	//tRad =
-	uShunt = pid_controller(tTargetRad, tRad);
-	HEATINGCTRLDEBUG("uShunt: %d\n", uShunt);
-	//setPWM(uShunt);
+        int8_t ret;
 
 
+        ow_rom_code_t romSensorRad;
+        HEATINGCTRLDEBUG("Sensor address:\n");
+        HEATINGCTRLDEBUG("Test: 0x%x\n",0x23);
+        for(i=0;i<8;i++){
+                romSensorRad.bytewise[i] = addr[i];
+        }
+        //romSensorRad.raw = 0x105602a501080011;
+        for(i=0;i<8;i++){
+                HEATINGCTRLDEBUG ("%x%x",romSensorRad.bytewise[i],addr[i]);
+        }
+        HEATINGCTRLDEBUG("\n");
+
+        tTargetRad = 40;
+        ret=ow_temp_start_convert_wait(&romSensorRad);
+        HEATINGCTRLDEBUG ("conv %d\n",ret);
+
+        ow_temp_scratchpad_t sp;
+        ret = ow_temp_read_scratchpad(&romSensorRad, &sp);
+
+        if (ret != 1)
+        {
+                HEATINGCTRLDEBUG("scratchpad read failed: %d\n", ret);
+        }
+        else{	HEATINGCTRLDEBUG("successfully read scratchpad\n");
 
 
-  return ECMD_FINAL_OK;
+        int16_t temp = ow_temp_normalize(&romSensorRad, &sp);
+
+        HEATINGCTRLDEBUG("temperature: %d.%d\n", HI8(temp), LO8(temp) > 0 ? 5 : 0);
+        HEATINGCTRLDEBUG("temperature: %d.%d\n", HI8(temp), HI8(((temp & 0x00ff) * 10) + 0x80));
+
+        //self.s.send("1w convert" + "\n")
+        //self.s.send("1w get " + sensorID + "\n")
+        //response = float(self.s.recv(1024).rstrip("\n").lstrip())
+
+        tRad = HI8(temp);
+        uShunt = pid_controller(tTargetRad, tRad);
+
+        HEATINGCTRLDEBUG("uShunt: %d\n", uShunt);
+
+        setpwm('b',(uint8_t) uShunt);
+
+
+        }
+
+        return ECMD_FINAL_OK;
 }
 
 /*
   This function will be called on request by menuconfig, if wanted...
   You need to enable ECMD_SUPPORT for this.
   Otherwise you can use this function for anything you like
-*/
+ */
 int16_t
 heating_ctrl_onrequest(char *cmd, char *output, uint16_t len){
-	HEATINGCTRLDEBUG ("main\n");
-	heating_ctrl_periodic();
-  // enter your code here
+        HEATINGCTRLDEBUG ("main\n");
+        heating_ctrl_periodic();
+        // enter your code here
 
-  return ECMD_FINAL_OK;
+        return ECMD_FINAL_OK;
 }
 
 /*
@@ -116,7 +137,7 @@ heating_ctrl_onrequest(char *cmd, char *output, uint16_t len){
  */
 int16_t
 pid_controller(int16_t tTarget, int16_t tMeasured){
-	return (tTarget-tMeasured) + 127;
+        return (tTarget-tMeasured) + 127;
 }
 
 
@@ -124,5 +145,5 @@ pid_controller(int16_t tTarget, int16_t tMeasured){
   -- Ethersex META --
   header(services/heating_controller/heating_ctrl.h)
   ifdef(`conf_HEATING_CTRL_INIT_AUTOSTART',`init(heating_ctrl_init)')
-  ifdef(`conf_HEATING_CTRL_PERIODIC_AUTOSTART',`timer(1000,heating_ctrl_periodic())')
-*/
+  ifdef(`conf_HEATING_CTRL_PERIODIC_AUTOSTART',`timer(500,heating_ctrl_periodic())')
+ */
